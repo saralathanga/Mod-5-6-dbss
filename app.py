aa887274f558de02a3af9bd6a1558dc39f7be214
Sara <119824878+saralathanga@users.noreply.github.com>
2025-07-14 06:56:56 +0000

    U1

--- app.py
+++ app.py
@@ -5,22 +5,26 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return(render_template("index copy.html"))
+    return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    # db
+    return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
 
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
-    
-    # return(render_template("prediction.html",r=(-50.6*q)+90.2))
-
     # load model
     model = joblib.load("dbs.jl")
-
     # make prediction
     pred = model.predict([[q]])
-
     return(render_template("prediction.html",r=pred))
 
 if __name__ == "__main__":
     app.run()
-
