4c3f26b8691b2e01912794e881adf7ddddf9f993
Sara <119824878+saralathanga@users.noreply.github.com>
2025-07-17 02:23:15 +0000

    U13

--- app.py
+++ app.py
@@ -94,9 +94,25 @@ def telegram():
     
     return(render_template("telegram.html", status=status))
 
+@app.route("/stop_telegram",methods=["GET","POST"])
+def stop_telegram():
+
+    domain_url = 'https://dbss-ezln.onrender.com'
+
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
+    webhook_response = requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+    # Set the webhook URL for the Telegram bot
+    if webhook_response.status_code == 200:
+        # set status message
+        status = "The telegram bot has stop."
+    else:
+        status = "Failed to stop the telegram bot."
+    return(render_template("stop_telegram.html", r=status))
+
 @app.route("/webhook",methods=["GET","POST"])
 def webhook():
-
+    
     # This endpoint will be called by Telegram when a new message is received
     update = request.get_json()
     if "message" in update and "text" in update["message"]:
@@ -124,23 +140,6 @@ def webhook():
             "text": response_message
         })
     return('ok', 200)
-
-@app.route("/stop_telegram",methods=["GET","POST"])
-def stop_telegram():
-
-    domain_url = 'https://dbss-ezln.onrender.com'
-
-    # The following line is used to delete the existing webhook URL for the Telegram bot
-    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
-    webhook_response = requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
-
-    if webhook_response.status_code == 200:
-        # set status message
-        status = "The telegram bot is stopped. "
-    else:
-        status = "Failed to stop the telegram bot. Please check the logs."
-    
-    return(render_template("telegram.html", status=status))
 
 if __name__ == "__main__":
     app.run()
